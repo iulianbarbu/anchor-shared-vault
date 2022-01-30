cc5c9933567caf121523f63465cef8ded0c1498c
Iulian Barbu <iulianbarbu1996@gmail.com>
2022-01-30 23:22:26 +0200

    Added primitives for initialize shared vault...
    ... deposit and withdraw money.

--- programs/anchor-shared-vault/src/lib.rs
+++ programs/anchor-shared-vault/src/lib.rs
@@ -1,14 +1,193 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, SetAuthority, TokenAccount, Transfer};
+use spl_token::instruction::AuthorityType;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3eC59jD55nX6HvYj6tE7ycbmj8ctBiGuCjLBvK6oFNz9");
 
 #[program]
 pub mod anchor_shared_vault {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    const SHARED_VAULT_PDA_SEED: &[u8] = b"shared-vault";
+
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        _vault_account_bump: u8,
+        initializer_amount: u64
+    ) -> ProgramResult {
+        ctx.accounts.shared_vault_account.initializer_key = *ctx.accounts.initializer.key;
+        ctx.accounts.shared_vault_account.amount = initializer_amount;
+
+        let (shared_vault_token_account_authority, _shared_vault_token_account_authority_bump) =
+            Pubkey::find_program_address(&[SHARED_VAULT_PDA_SEED], ctx.program_id);
+
+        token::set_authority(
+            ctx.accounts.into_set_authority_context(),
+            AuthorityType::AccountOwner,
+            Some(shared_vault_token_account_authority),
+        )?;
+
+        token::transfer(
+            ctx.accounts.into_transfer_to_pda_context(),
+            ctx.accounts.shared_vault_account.amount,
+        )?;
+        
+        Ok(())
+    }
+
+    pub fn deposit(
+        ctx: Context<Deposit>,
+        amount: u64
+    ) -> ProgramResult {
+        ctx.accounts.shared_vault_account.amount += amount;
+        token::transfer(
+            ctx.accounts.into_transfer_to_pda_context(),
+            amount
+        )?;
+
+        Ok(())
+    }
+
+    pub fn withdraw(
+        ctx: Context<Withdraw>,
+        amount: u64
+    ) -> ProgramResult {
+        let (_vault_authority, vault_authority_bump) =
+        Pubkey::find_program_address(&[SHARED_VAULT_PDA_SEED], ctx.program_id);
+        let authority_seeds = &[&SHARED_VAULT_PDA_SEED[..], &[vault_authority_bump]];
+
+        ctx.accounts.shared_vault_account.amount -= amount;
+        token::transfer(
+            ctx.accounts
+                .into_transfer_to_user_token_account()
+                .with_signer(&[&authority_seeds[..]]),
+            amount
+        )?;
+
         Ok(())
     }
 }
 
+#[account]
+pub struct SharedVaultAccount {
+    pub initializer_key: Pubkey,
+    pub amount: u64,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(shared_vault_token_account_bump: u8, initializer_amount: u64)]
+pub struct Initialize<'info> {
+    #[account(mut, signer)]
+    pub initializer: AccountInfo<'info>,
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init,
+        seeds = [b"token-seed".as_ref()],
+        bump = shared_vault_token_account_bump,
+        payer = initializer,
+        token::mint = mint,
+        token::authority = initializer,
+    )]
+    pub shared_vault_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint = initializer_token_account.amount >= initializer_amount
+    )]
+    pub initializer_token_account: Account<'info, TokenAccount>,
+    #[account(zero)]
+    pub shared_vault_account: Box<Account<'info, SharedVaultAccount>>,
+    pub system_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub token_program: AccountInfo<'info>,
+}
+
+impl<'info> Initialize<'info> {
+    fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self
+                .initializer_token_account
+                .to_account_info()
+                .clone(),
+            to: self.shared_vault_token_account.to_account_info().clone(),
+            authority: self.initializer.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.shared_vault_token_account.to_account_info().clone(),
+            current_authority: self.initializer.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(amount: u64)]
+pub struct Deposit<'info> {
+    #[account(mut, signer)]
+    pub user: AccountInfo<'info>,
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub shared_vault_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint = user_token_account.amount >= amount
+    )]
+    pub user_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub shared_vault_account: Account<'info, SharedVaultAccount>,
+    pub system_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub token_program: AccountInfo<'info>,
+}
+
+impl<'info> Deposit<'info> {
+    fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self
+                .user_token_account
+                .to_account_info()
+                .clone(),
+            to: self.shared_vault_token_account.to_account_info().clone(),
+            authority: self.user.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(amount: u64)]
+pub struct Withdraw<'info> {
+    #[account(mut, signer)]
+    pub user: AccountInfo<'info>,
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub shared_vault_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub user_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint = shared_vault_account.amount >= amount
+    )]
+    pub shared_vault_account: Account<'info, SharedVaultAccount>,
+    pub shared_vault_token_account_authority: AccountInfo<'info>,
+    pub system_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub token_program: AccountInfo<'info>,
+}
+
+impl<'info> Withdraw<'info> {
+    fn into_transfer_to_user_token_account(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self
+                .shared_vault_token_account
+                .to_account_info()
+                .clone(),
+            to: self.user_token_account.to_account_info().clone(),
+            authority: self.shared_vault_token_account_authority.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
--- tests/anchor-shared-vault.ts
+++ tests/anchor-shared-vault.ts
@@ -1,17 +1,207 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { AnchorSharedVault } from '../target/types/anchor_shared_vault';
+import { PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+import { assert } from "chai";
 
 describe('anchor-shared-vault', () => {
-
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorSharedVault as Program<AnchorSharedVault>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  const initializerAmount = 500;
+  const depositAmount = 200;
+  const withdrawAmount = 100;
+  const sharedVaultAccount = anchor.web3.Keypair.generate();
+  const initializerMainAccount = anchor.web3.Keypair.generate();
+  const depositerMainAccount = anchor.web3.Keypair.generate();
+
+  const payer = anchor.web3.Keypair.generate();
+  const mintAuthority = anchor.web3.Keypair.generate();
+
+  let mint = null;
+  let initializerTokenAccount = null;
+  let depositerTokenAccount = null;
+  let sharedVaultTokenAccountPDA = null;
+  let sharedVaultTokenAccountBump = null;
+  let sharedVaultTokenAccountAuthorityPDA = null;
+
+  it("Initialize program state", async () => {
+    // Airdropping tokens to a payer.
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, 10 ** 9),
+      "confirmed"
+    );
+
+    // Fund Main Account
+    await provider.send(
+      (() => {
+        const tx = new Transaction();
+        tx.add(
+          SystemProgram.transfer({
+            fromPubkey: payer.publicKey,
+            toPubkey: initializerMainAccount.publicKey,
+            lamports: 10 ** 8,
+          }),
+          SystemProgram.transfer({
+            fromPubkey: payer.publicKey,
+            toPubkey: depositerMainAccount.publicKey,
+            lamports: 10 ** 8,
+          })
+        );
+        return tx;
+      })(),
+      [payer]
+    );
+
+    mint = await Token.createMint(
+      provider.connection,
+      payer,
+      mintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
+    initializerTokenAccount = await mint.createAccount(initializerMainAccount.publicKey);
+    await mint.mintTo(
+      initializerTokenAccount,
+      mintAuthority.publicKey,
+      [mintAuthority],
+      initializerAmount
+    );
+
+    depositerTokenAccount = await mint.createAccount(depositerMainAccount.publicKey);
+    await mint.mintTo(
+      depositerTokenAccount,
+      mintAuthority.publicKey,
+      [mintAuthority],
+      depositAmount * 2
+    );
+
+    let _initializerTokenAccount = await mint.getAccountInfo(initializerTokenAccount);
+    assert.ok(_initializerTokenAccount.amount.toNumber() == initializerAmount);
+
+    let _depositerTokenAccount = await mint.getAccountInfo(depositerTokenAccount);
+    assert.ok(_depositerTokenAccount.amount.toNumber() == depositAmount * 2);
+  });
+
+  it("Initialize shared vault", async () => {
+    const [_sharedVaultTokenAccountPDA, _sharedVaultTokenAccountBump] = await PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("token-seed"))],
+      program.programId
+    );
+
+    sharedVaultTokenAccountPDA = _sharedVaultTokenAccountPDA;
+    sharedVaultTokenAccountBump = _sharedVaultTokenAccountBump;
+
+    const [_sharedVaultTokenAccountAuthorityPDA, _sharedVaultTOkenAccountAuthorityBump] =
+     await PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("shared-vault"))],
+      program.programId
+    );
+    sharedVaultTokenAccountAuthorityPDA = _sharedVaultTokenAccountAuthorityPDA;
+
+    await program.rpc.initialize(
+      sharedVaultTokenAccountBump,
+      new anchor.BN(initializerAmount),
+      {
+        accounts: {
+          initializer: initializerMainAccount.publicKey,
+          sharedVaultTokenAccount: sharedVaultTokenAccountPDA,
+          mint: mint.publicKey,
+          initializerTokenAccount: initializerTokenAccount,
+          sharedVaultAccount: sharedVaultAccount.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        },
+        instructions: [
+          await program.account.sharedVaultAccount.createInstruction(sharedVaultAccount),
+        ],
+        signers: [sharedVaultAccount, initializerMainAccount],
+      }
+    );
+    
+    let _vault = await mint.getAccountInfo(sharedVaultTokenAccountPDA);
+
+    let _sharedVaultAccount = await program.account.sharedVaultAccount.fetch(
+      sharedVaultAccount.publicKey
+    );
+
+    // Check that the new owner is the PDA.
+    assert.ok(_vault.owner.equals(sharedVaultTokenAccountAuthorityPDA));
+
+    // Check that the values in the escrow account match what we expect.
+    assert.ok(_sharedVaultAccount.initializerKey.equals(initializerMainAccount.publicKey));
+    assert.ok(_sharedVaultAccount.amount.toNumber() == initializerAmount);
+  });
+
+  it("Deposit money", async () => {
+    await program.rpc.deposit(
+      new anchor.BN(depositAmount),
+      {
+        accounts: {
+          user: depositerMainAccount.publicKey,
+          sharedVaultTokenAccount: sharedVaultTokenAccountPDA,
+          mint: mint.publicKey,
+          userTokenAccount: depositerTokenAccount,
+          sharedVaultAccount: sharedVaultAccount.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        },
+        signers: [depositerMainAccount],
+      }
+    );
+    
+    let _vault = await mint.getAccountInfo(sharedVaultTokenAccountPDA);
+
+    let _sharedVaultAccount = await program.account.sharedVaultAccount.fetch(
+      sharedVaultAccount.publicKey
+    );
+
+    // Check that the new owner is the PDA.
+    assert.ok(_vault.owner.equals(sharedVaultTokenAccountAuthorityPDA));
+
+    // Check that the values in the escrow account match what we expect.
+    assert.ok(_sharedVaultAccount.initializerKey.equals(initializerMainAccount.publicKey));
+    assert.ok(_sharedVaultAccount.amount.toNumber() == initializerAmount + depositAmount);
+  });
+
+  it("Withdraw money", async () => {
+    await program.rpc.withdraw(
+      new anchor.BN(withdrawAmount),
+      {
+        accounts: {
+          user: depositerMainAccount.publicKey,
+          sharedVaultTokenAccount: sharedVaultTokenAccountPDA,
+          mint: mint.publicKey,
+          userTokenAccount: depositerTokenAccount,
+          sharedVaultAccount: sharedVaultAccount.publicKey,
+          sharedVaultTokenAccountAuthority: sharedVaultTokenAccountAuthorityPDA,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        },
+        signers: [depositerMainAccount],
+      }
+    );
+    
+    let _vault = await mint.getAccountInfo(sharedVaultTokenAccountPDA);
+
+    let _sharedVaultAccount = await program.account.sharedVaultAccount.fetch(
+      sharedVaultAccount.publicKey
+    );
+
+    // Check that the new owner is the PDA.
+    assert.ok(_vault.owner.equals(sharedVaultTokenAccountAuthorityPDA));
+
+    // Check that the values in the escrow account match what we expect.
+    assert.ok(_sharedVaultAccount.initializerKey.equals(initializerMainAccount.publicKey));
+    assert.ok(initializerAmount + depositAmount - withdrawAmount  ==_sharedVaultAccount.amount.toNumber());
   });
 });
